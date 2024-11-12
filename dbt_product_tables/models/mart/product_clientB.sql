610cdc6af327f45b4c76b53213755114c5c9303b
naivm <36080596+naivm@users.noreply.github.com>
2024-11-12 19:53:16 +0000

    filters updated and split by client (#10)

--- dbt_product_tables/models/mart/product_clientB.sql
+++ dbt_product_tables/models/mart/product_clientB.sql
@@ -1,15 +1,15 @@
 WITH material_info AS (
     SELECT
-        LPAD(CAST(mara.MATNR AS STRING), 18, '0') AS product_id,
-        mara.MATKL     AS product_category,
+        LPAD(CAST(mara.MATNR AS STRING), 18, '0') AS product_ids,
         mara.lvorm     AS product_status,
         mara.mtart     AS material_type,
         mara.mbrsh     AS industry_sector,
         mara.brand_id  AS brand_id,
+        cast(mara.MATKL as STRING) AS product_category,
         -- Classify client type based on product category (MATKL) in MARA
         CASE
-            WHEN CONCAT('abc', CAST(mara.MATKL AS STRING)) IN ('abc002', 'abc00207', 'abc00103') THEN 'Client A'
-            WHEN CONCAT('abc', CAST(mara.MATKL AS STRING)) IN ('abc001', 'abc004') THEN 'Client B'
+            WHEN CONCAT('abc', mara.MATKL) IN ('abc002', 'abc00207', 'abc00103') THEN 'Client A'
+            WHEN CONCAT('abc', mara.MATKL) IN ('abc001', 'abc004') THEN 'Client B'
             ELSE 'Other'
         END AS client_type
     FROM
@@ -23,10 +23,17 @@ WITH material_info AS (
         {{ ref('int_makt') }} AS makt
 )
 
-select
-    *
+select DISTINCT
+    material_info.product_category,
+    material_info.product_status,
+    material_info.material_type,
+    material_info.industry_sector,
+    material_info.brand_id,
+    material_info.client_type,
 from
     material_info
 left JOIN porduct_info
 on
-    material_info.product_id = porduct_info.product_id
+    material_info.product_ids = porduct_info.product_id
+WHERE
+    material_info.client_type = 'Client B'
