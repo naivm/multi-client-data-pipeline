05f7f595adeca415474ee856f67fdabce758c403
naivm <36080596+naivm@users.noreply.github.com>
2024-11-12 20:08:00 +0000

    split and filtered client model (#11)

---
+++ dbt_product_tables/models/mart/clientA.sql
@@ -0,0 +1,15 @@
+with transaction_a as (
+  select DISTINCT
+    customer_id
+  from
+    {{ ref('transaction_clientA')}} as tran_a
+)
+
+select
+    uwaer                         AS sales_currency,
+    land1                         AS customer_country,
+    ort01                         AS customer_city,
+    ktokd                         AS customer_group,
+from {{ref('int_kna1')}} as kna1
+  right join transaction_a
+    on kna1.kunnr = transaction_a.customer_id
