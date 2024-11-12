17ea95f728dbd3f982bcb666aedf80b1f9677d78
miguel vian <miguel.vian@live.com.pt>
2024-11-12 20:07:17 +0000

    split and filtered client model

--- dbt_product_tables/models/mart/client.sql
+++
@@ -1,9 +0,0 @@
-select
-    mandt                         AS client,
-    kunnr                         AS customer_id,
-    uwaer                         AS sales_currency,
-    land1                         AS client_country,
-    ort01                         AS client_city,
-    ktokd                         AS client_group,
-
-from {{ref('int_kna1')}}
